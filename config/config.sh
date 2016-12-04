3aa1c84fda9d9f78a0d88a2b8cfce5f4df94f869
dongchangzhang <dongchangzhang@gmail.com>
2016-12-04 22:39:07 +0800

    use my vim config && use another zsh themes

--- config/config.sh
+++ config/config.sh
@@ -2,16 +2,24 @@
 
 
 # zsh
+
+git clone https://github.com/caiogondim/bullet-train-oh-my-zsh-theme.git
+cd bullet-train-oh-my-zsh-theme
 mv ~/.zshrc ~/.zshrc_back
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 cp my_zshrc ~/.zshrc
+cp bullet-train.zsh-theme ~/.oh-my-zsh/themes/
+cd -
 chsh -s /bin/zsh
 
 # vim
-sh spf13-vim.sh
+mv ~/.vim .vim_backup
+mv ~/.vimrc .vimrc_backup
 
-cp vimrc.local ~/.vimrc.local
-cp vimrc.before.local ~/.vimrc.before.local
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp my_vimrc ~/.vimrc
+vim +PluginInstall +qall
+
 
 # powerline font 
 git clone https://github.com/powerline/fonts.git
